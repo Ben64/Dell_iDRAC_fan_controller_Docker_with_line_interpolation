c01f2c311ae476fdc670fd9229b436eb706a00b2
Ben Sixtyfour <git@ben64.com>
2024-02-01 22:59:09 -0800

    test

--- Dell_iDRAC_fan_controller.sh
+++ Dell_iDRAC_fan_controller.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#pp
 # Enable strict bash mode to stop the script if an uninitialized variable is used, if a command fails, or if a command with a pipe fails
 # Not working in some setups : https://github.com/tigerblue77/Dell_iDRAC_fan_controller/issues/48
 # set -euo pipefail
